228b9785d2a0b22203fe1f959f5dbfcafcbc4ab6
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2024-11-17 13:26:29 -0500

    Update App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,10 @@
+// src/tests/App.test.js
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = document.querySelector('a');
+  expect(linkElement).toBeInTheDocument();
+});
 
